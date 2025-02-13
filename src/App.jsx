ccb19c8987506e3a15d87d6abe34bf1e5b3ac00b
zoxibe <titi.franville@gmail.com>
2025-02-13 02:09:04 +0100

    BigUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -77,56 +77,99 @@ function App() {
             <div className={"Sommaire1"}>
                 <Sommaire/>
             </div>
+
+
             <div className={"header"}>
                 {/*<Cursor/>*/}
                 <button className={"ColorTheme"} onClick={myFunction}></button>
                 <div className={"title"}>
                     <h2 id="Title">TiméoF</h2>
                 </div>
-                <div className={"ScrolingBar"}>
+                <div className={"AnimScrolingBar"}>
                     <div className={"ScrolingBarInner"}/>
+                </div>
+                <div className={"ScrollingBar"}>
+                    <p className={"ScrollDown"}>* SCROLL DOWN *</p>
                 </div>
             </div>
 
-            <div className={"container"}>
-                 <p className={"ScrollDown"}>* SCROLL DOWN *</p>
-            </div>
-            <div className={"TextBox"}>
-                <div className={"flip-card"} id="flip-card">
-                    <div className={"flip-card-inner"}>
-                        <div className={"flip-card-front"}>
-                            <img src={FaceImg} alt={"Avatar"} width="100%" height="100%"/>
-                        </div>
-                        <div className={"flip-card-back"}>
-                            <h3>Timéo FRANVILLE</h3>
-                            <h4>Étudiant en 2ᵉ année à <a href={"https://www.esiea.fr"} target="_blank" style={{color:"rgb(54,169,255)"}}>l’ESIEA</a></h4>
-                            <h5>École d’ingénieurs spécialisée dans les technologies du numérique</h5>
-                        </div>
+
+            <div className={"AboutMe"}>
+
+                <div className={"card"}>
+                    <div className={"Card-up"}>
+                        <h3>Timéo FRANVILLE</h3>
+                        <h4>Étudiant en 2ᵉ année à <a href={"https://www.esiea.fr"} target="_blank"
+                                                      style={{color: "rgb(54,169,255)"}}>l’ESIEA</a></h4>
+                    </div>
+
+                    <div className={"Card-down"}>
+                        <h5>École d’ingénieurs spécialisée dans les technologies du numérique</h5>
                     </div>
                 </div>
-                <div className={"Text1"}>
-                    <p>je recherche un contrat d’apprentissage dans le domaine de la réalité augmentée pour allier compétences techniques et passion pour l’innovation.</p>
+
+                <div className={"ZoneText"}>
+                    <div className={"Text1"}>
+                        <p>Découvrir</p>
+                        <div className={"Image1"}></div>
+                    </div>
+                    <div className={"Text2"}>
+                        <div className={"Image2"}></div>
+                        <p>Créer</p>
+                    </div>
+                    <div className={"Text3"}>
+                        <p>Partager</p>
+                        <div className={"Image3"}></div>
+                    </div>
                 </div>
-                <div className={"Text2"}>
-                    <p>J'ai une forte curiosité pour les technologies de réalité augmentée et virtuelle ainsi que les système immersifs. J'apprécie également partager et créer, ce que je mets en avant lors de mes <a href={"https://www.twitch.tv/zoxibe"} target="_blank">streams</a>.</p>
+
+
+                <div className={"ScrollingBar2"}>
+                    <p className={"ScrollDown2"}>* À PROPOS DE MOI *</p>
                 </div>
-                <div className={"Text3"}>
-                    <p style={{fontSize: "4vw", fontFamily: "'Corinthia', serif", textDecoration:"underline", textDecorationThickness: "1px"}}>Compétences techniques</p>
-                    <ol style={{listStyleType: "disc"}}>
-                        <li style={{margin: "0 13vw"}}>Programmation : [C, C++, JavaScript, Css]</li>
-                    </ol>
-                    <p style={{fontSize: "4vw", fontFamily: "'Corinthia', serif",textDecoration:"underline", textDecorationThickness: "1px"}}>Compétences personnelles</p>
-                    <ol  style={{listStyleType: "disc"}}>
-                        <li style={{margin: "0 13vw"}}>Capacité à travailler en équipe.</li>
-                        <li style={{margin: "0 13vw"}}>Adaptabilité et curiosité pour les nouvelles technologies.</li>
-                        <li style={{margin: "0 13vw"}}>Sens de l’organisation et respect des délais</li>
-                    </ol>
-                </div>
+
+
             </div>
+
+            <div className={"Compétences"}>
+
+                <div className={"imageCompétences"}></div>
+
+                <div className={"TextCompétences"}>
+                    <p>Compétences techniques</p>
+                    <ol style={{listStyleType: "disc"}}>
+                        <li>Programmation : [C, C++, JavaScript, Css]</li>
+                    </ol>
+                    <p>Compétences personnelles</p>
+                    <ol style={{listStyleType: "disc"}}>
+                        <li>Capacité à travailler en équipe.</li>
+                        <li>Adaptabilité et curiosité pour les nouvelles technologies.</li>
+                        <li>Sens de l’organisation et respect des délais.</li>
+                    </ol>
+                </div>
+
+                <div className={"ScrollingBar3"}>
+                    <p className={"ScrollDown3"}>* MES COMPÉTENCES *</p>
+                </div>
+
+            </div>
+
+            <div className={"Search"}>
+
+
+                <div className={"ScrollingBar4"}>
+                    <p className={"ScrollDown4"}>* JE RECHERCHE *</p>
+                </div>
+
+                <div className={"SearchText1"}></div>
+
+            </div>
+
+
             <div className={"Input"}>
                 <div>
                     <h1 id="Contact">
-                    Contact
+                        Contact
                     </h1>
                 </div>
                 <ContactBox/>
@@ -135,7 +178,7 @@ function App() {
                 <a href="#">Design & Coding by Timéo © 2024.</a>
             </div>
         </>
-    )
+        )
 }
 
 export default App;
